136e15fc32714ae381b365d5b11f54fd15d763a5
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2020-10-23 14:15:05 +0300

    Start screen setup

---
+++ ATM-Interface/ViewModel/StartScreenViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_Interface.ViewModel
+{
+    class StartScreenViewModel
+    {
+        public StartScreenViewModel()
+        {
+
+        }
+    }
+}
